62a9559b290af45cbf11a0c48f3eb1e2fb2e22d1
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-09-25 19:56:14 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "pbs.twimg.com",
-      },
-    ],
-  },
   async redirects() {
     return [
       {
